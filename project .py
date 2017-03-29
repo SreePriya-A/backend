e443a6219fe0e40280228f62e204090fb84bc900
SriKaavya <srikaavya.toodi@gmail.com>
2017-03-29 21:39:01 +0530

    Update project .py

--- project .py
+++ project .py
@@ -3,7 +3,7 @@ getnode(123467809) #just to call a method
 def getnode(Id):
     db = MySQLdb.connect("localhost","Sri","Sri@1204","sample" )
     cursor = db.cursor()
-    cursor.execute("SELECT parent from dummy where id=Id")
+    cursor.execute("SELECT parent from dummy where id="+Id)
     data = cursor.fetchone()
     if data>100000000:
       getnode(data)
@@ -14,13 +14,13 @@ def getnode(Id):
 def getlocation(Node):
     db = MySQLdb.connect("localhost","Sri","Sri@1204","sample" )
     cursor = db.cursor()
-    cursor.execute("SELECT location from locMap where node=Node")
+    cursor.execute("SELECT location from locMap where node="+Node)
     data = cursor.fetchone()
     db.close()
     return data
 def updatenode(Id, Node):
     db = MySQLdb.connect("localhost","Sri","Sri@1204","sample" )
     cursor = db.cursor()
-    cursor.execute("UPDATE dummy SET parent =Node WHERE id =Id")
+    cursor.execute("UPDATE dummy SET parent =Node WHERE id ="+Id)
     db.close()
     
